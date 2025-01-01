422c7021474f3c5e1d70a8fd486b517a69792471
luloduarte <duarte.castineira@campus.tu-berlin.de>
2025-01-01 10:18:25 +0100

    First small test StingsTest.cpp to try

--- test/runtime/local/datastructures/StringsTest.cpp
+++ test/runtime/local/datastructures/StringsTest.cpp
@@ -1,7 +1,6 @@
 #include <runtime/local/datastructures/DataObjectFactory.h>
 #include <runtime/local/datastructures/DenseMatrix.h>
 #include <runtime/local/datastructures/ValueTypeUtils.h>
-#include <runtime/local/io/File.h>
 #include <runtime/local/io/ReadCsv.h>
 #include <runtime/local/kernels/EwBinarySca.h>
 
@@ -11,7 +10,9 @@
 
 #include <cstdint>
 
-TEMPLATE_PRODUCT_TEST_CASE("Check Dimensions of CSV", TAG_IO, (DenseMatrix), (std::string)) {
+#define TEST_NAME(opName) "Strings (" opName ")"
+
+TEMPLATE_PRODUCT_TEST_CASE(TEST_NAME("ReadCsv"), TAG_IO, (DenseMatrix), (ALL_STRING_VALUE_TYPES)) {
     using DT = TestType;
     DT *m = nullptr;
 
@@ -27,4 +28,27 @@ TEMPLATE_PRODUCT_TEST_CASE("Check Dimensions of CSV", TAG_IO, (DenseMatrix), (st
     REQUIRE(m->getNumCols() == numCols);
 
     DataObjectFactory::destroy(m);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(TEST_NAME("eq"), TAG_KERNELS, (DenseMatrix), (ALL_STRING_VALUE_TYPES)) {
+    using DT = TestType;
+    DT *m1 = nullptr;
+
+    using DT = TestType;
+    DT *m2 = nullptr;
+
+    size_t numRows = 5000;
+    size_t numCols = 5;
+
+    char filename[] = "./test/data/strings/uniform_synthetic_random_strings.csv";
+    char delim = ',';
+
+    readCsv(m, filename, numRows, numCols, delim);
+    readCsv(m, filename, numRows, numCols, delim);
+
+    DTRes *res = nullptr;
+    ewBinaryMat<DTRes, DTArg, DTArg>((BinaryOpCode::EQ, res, m1, m2, nullptr);
+
+    DataObjectFactory::destroy(m1);
+    DataObjectFactory::destroy(m2);
 }
