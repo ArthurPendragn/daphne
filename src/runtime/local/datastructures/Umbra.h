a9d0905715f626bbe072552bc826ac6950b3731f
luloduarte <duarte.castineira@campus.tu-berlin.de>
2025-01-02 08:59:43 +0100

    First small test StingsTest.cpp to try

--- src/runtime/local/datastructures/Umbra.h
+++ src/runtime/local/datastructures/Umbra.h
@@ -154,7 +154,32 @@ class Umbra_t {
     bool operator!=(const char *str) const { return !(*this == str); }
 
     // Less-than comparison with other Umbra Strings
-    bool operator<(const Umbra_t &other) const { return true; }
+    bool operator<(const Umbra_t &other) const {
+        uint32_t min_length = std::min(length, other.length);
+        int cmp;
+        if (length <= 12 && other.length <= 12) {
+            cmp = std::memcmp(short_str, other.short_str, min_length);
+        } else if (length <= 12) {
+            cmp = std::memcmp(short_str, other.long_str.prefix, 4);
+            if (cmp == 0) {
+                cmp = std::memcmp(short_str, other.long_str.ptr, min_length);
+            }
+        } else if (other.length <= 12) {
+            cmp = std::memcmp(long_str.prefix, other.short_str, 4);
+            if (cmp == 0) {
+                cmp = std::memcmp(long_str.ptr, other.short_str, min_length);
+            }
+        } else {
+            cmp = std::memcmp(long_str.prefix, other.long_str.prefix, 4);
+            if (cmp == 0) {
+                cmp = std::memcmp(long_str.ptr, other.long_str.ptr, min_length);
+            }
+        }
+        if (cmp == 0) {
+            return length < other.length;
+        }
+        return cmp < 0;
+    }
 
     // Less-than comparison with other C-style strings
     bool operator<(const char *str) const {
