9a53e0e5ae647a5667af00740bdb7a2f487db786
luloduarte <duarte.castineira@campus.tu-berlin.de>
2025-01-06 23:41:19 +0100

    Looking for performance improvements

--- test/runtime/local/datastructures/StringsTest.cpp
+++ test/runtime/local/datastructures/StringsTest.cpp
@@ -122,18 +122,20 @@ TEMPLATE_PRODUCT_TEST_CASE(TEST_NAME("Uniform(2-11) - EwBinaryMat"), TAG_DATASTR
     REQUIRE(m2->getNumCols() == NUM_COLS);
 
     SECTION("Test") {
-        EwBinaryScaFuncPtr<int64_t, VT, VT> func = getEwBinaryScaFuncPtr<int64_t, VT, VT>(BinaryOpCode::EQ);
-        DTRes *res = DataObjectFactory::create<DenseMatrix<int64_t>>(NUM_ROWS, NUM_COLS, false);
-        const VT *valuesLhs = m1->getValues();
-        const VT *valuesRhs = m2->getValues();
-        int64_t *valuesRes = res->getValues();
-        for (size_t r = 0; r < NUM_ROWS; r++) {
-            for (size_t c = 0; c < NUM_COLS; c++) {
-                valuesRes[c] = func(valuesLhs[c], valuesRhs[c], nullptr);
+        for (size_t i = 0; i < LOOP_SIZE; i++) {
+            EwBinaryScaFuncPtr<int64_t, VT, VT> func = getEwBinaryScaFuncPtr<int64_t, VT, VT>(BinaryOpCode::EQ);
+            DTRes *res = DataObjectFactory::create<DenseMatrix<int64_t>>(NUM_ROWS, NUM_COLS, false);
+            const VT *valuesLhs = m1->getValues();
+            const VT *valuesRhs = m2->getValues();
+            int64_t *valuesRes = res->getValues();
+            for (size_t r = 0; r < NUM_ROWS; r++) {
+                for (size_t c = 0; c < NUM_COLS; c++) {
+                    valuesRes[c] = func(valuesLhs[c], valuesRhs[c], nullptr);
+                }
+                valuesLhs += m1->getRowSkip();
+                valuesRhs += m2->getRowSkip();
+                valuesRes += res->getRowSkip();
             }
-            valuesLhs += m1->getRowSkip();
-            valuesRhs += m2->getRowSkip();
-            valuesRes += res->getRowSkip();
         }
     }
 
