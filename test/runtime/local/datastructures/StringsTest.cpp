cf9954f5247b7b4a9f1993d16230b5d72bd5e8ac
luloduarte <duarte.castineira@campus.tu-berlin.de>
2025-01-15 21:56:49 +0100

    Looking for Umbra improvements

--- test/runtime/local/datastructures/StringsTest.cpp
+++ test/runtime/local/datastructures/StringsTest.cpp
@@ -27,7 +27,7 @@
 
 #define LOOP_SIZE 1000
 #define NUM_COLS 5
-#define NUM_ROWS 1
+#define NUM_ROWS 10
 #define TEST_FILE_1 "./test/data/strings/uniform_synthetic_random_strings.csv"
 
 #define DELIM ','
